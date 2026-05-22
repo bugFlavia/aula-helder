35e20d5b4296ff54609a72afade75d5dd8c93476
Flavia Nunes <flavia.morais@estudante.ifms.edu.br>
2026-05-22 15:59:26 -0400

    estrutura

---
+++ repositories/Emprestimo.js
@@ -0,0 +1,56 @@
+const { conectar, getDatabase } = require("../db/MongoClient.js");
+
+class Emprestimo {
+  constructor() {
+    this.colecao = getDatabase("biblioteca").collection("emprestimos");
+  }
+
+  async cadastrarEmprestimo(emprestimo) {
+    try {
+      const resultado = await this.colecao.insertOne(emprestimo);
+      return resultado;
+    } catch (error) {
+      console.error("Erro ao cadastrar empréstimo:", error);
+      throw error;
+    }
+  }
+  async listarEmprestimos() {
+    try {
+      const emprestimos = await this.colecao.find().toArray();
+      return emprestimos;
+    } catch (error) {
+      console.error("Erro ao listar empréstimos:", error);
+      throw error;
+    }
+  }
+  async buscarEmprestimoPorId(id) {
+    try {
+      const emprestimo = await this.colecao.findOne({ _id: new ObjectId(id) });
+      return emprestimo;
+    } catch (error) {
+      console.error("Erro ao buscar empréstimo por ID:", error);
+      throw error;
+    }
+  }
+  async atualizarEmprestimo(id, emprestimoAtualizado) {
+    try {
+      const resultado = await this.colecao.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: emprestimoAtualizado },
+      );
+      return resultado;
+    } catch (error) {
+      console.error("Erro ao atualizar empréstimo:", error);
+      throw error;
+    }
+  }
+  async excluirEmprestimo(id) {
+    try {
+      const resultado = await this.colecao.deleteOne({ _id: new ObjectId(id) });
+      return resultado;
+    } catch (error) {
+      console.error("Erro ao excluir empréstimo:", error);
+      throw error;
+    }
+  }
+}
--- repositories/Livro.js
+++ repositories/Livro.js
@@ -7,11 +7,50 @@ class Livro {
 
   async cadastrarLivro(livro) {
     try {
-      const resultado = this.colecao.insertOne(livro);
+      const resultado = await this.colecao.insertOne(livro);
       return resultado;
     } catch (error) {
-      console.log();
+      console.error("Erro ao cadastrar livro:", error);
+      throw error;
     }
   }
-  ...
+  async listarLivros() {
+    try {
+      const livros = await this.colecao.find().toArray();
+      return livros;
+    } catch (error) {
+      console.error("Erro ao listar livros:", error);
+      throw error;
+    }
+  }
+  async buscarLivroPorId(id) {
+    try {
+      const livro = await this.colecao.findOne({ _id: new ObjectId(id) });
+      return livro;
+    } catch (error) {
+      console.error("Erro ao buscar livro por ID:", error);
+      throw error;
+    }
+  }
+  async atualizarLivro(id, livroAtualizado) {
+    try {
+      const resultado = await this.colecao.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: livroAtualizado },
+      );
+      return resultado;
+    } catch (error) {
+      console.error("Erro ao atualizar livro:", error);
+      throw error;
+    }
+  }
+  async excluirLivro(id) {
+    try {
+      const resultado = await this.colecao.deleteOne({ _id: new ObjectId(id) });
+      return resultado;
+    } catch (error) {
+      console.error("Erro ao excluir livro:", error);
+      throw error;
+    }
+  }
 }
